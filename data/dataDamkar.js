86cc504ac415e1203877809c1fcb87f8744601f1
clumsyyyy <owencwijaya@gmail.com>
2021-08-11 11:30:19 +0700

    finalisasi fungsionalitas

---
+++ data/dataDamkar.js
@@ -0,0 +1,240 @@
+dataDamkar = [
+  {
+    "Nama": "Damkar Aceh",
+    "Alamat": "Jl. Soekarno - Hatta No.39, Geuceu Meunara, Kec. Jaya Baru, Kota Banda Aceh, Aceh 23232",
+    "Province": "Aceh",
+    "Provinsi": "Aceh",
+    "Nomor Telepon": "651-44123"
+  },
+  {
+    "Nama": "Damkar Medan",
+    "Alamat": "Jl. Candi Borobudur No. 2 Medan",
+    "Province": "North Sumatra",
+    "Provinsi": "Sumatra Utara",
+    "Nomor Telepon": "061-1134515356"
+  },
+  {
+    "Nama": "Damkar Padang",
+    "Alamat": "Jl. Bagindo Azis Chan No. 1 Aie Pacah - Kota Padang Sumatera Barat",
+    "Province": "West Sumatra",
+    "Provinsi": "Sumatra Barat",
+    "Nomor Telepon": "0751-4640800"
+  },
+  {
+    "Nama": "Damkar Riau",
+    "Alamat": "Jl. Cempaka No.31, Pulau Karam, Kec. Sukajadi, Kota Pekanbaru, Riau 28156",
+    "Province": "Riau",
+    "Provinsi": "Riau",
+    "Nomor Telepon": "0761-22382"
+  },
+  {
+    "Nama": "Damkar Kepulauan Riau",
+    "Alamat": "Jl. Daeng Marewa Kel. Senggarang Kec. Tanjungpinang Kota Kota Tanjungpinang - 29115 Provinsi Kepulauan Riau",
+    "Province": "Kepulauan Riau",
+    "Provinsi": "Kepulauan Riau",
+    "Nomor Telepon": "0771-733400405"
+  },
+  {
+    "Nama": "Damkar Jambi",
+    "Alamat": "Jl. Jend.Basuki Rahmat no.1 Kotabaru, Jambi - Indonesia 36128",
+    "Province": "Jambi",
+    "Provinsi": "Jambi",
+    "Nomor Telepon": "0741-444953"
+  },
+  {
+    "Nama": "Damkar Bengkulu",
+    "Alamat": "Sido Mulyo, Kec. Gading Cemp., Kota Bengkulu, Bengkulu 38211",
+    "Province": "Bengkulu",
+    "Provinsi": "Bengkulu",
+    "Nomor Telepon": "0736-51113"
+  },
+  {
+    "Nama": "Damkar Sumatera Selatan",
+    "Alamat": "Jl. Merdeka No.8, 19 Ilir, Kec. Bukit Kecil, Kota Palembang, Sumatera Selatan 30113",
+    "Province": "South Sumatra",
+    "Provinsi": "Sumatra Selatan",
+    "Nomor Telepon": "0711-312011"
+  },
+  {
+    "Nama": "Damkar Bangka Belitung",
+    "Alamat": "Kacang Pedang, Gerunggang, Kota Pangkal Pinang, Kepulauan Bangka Belitung 33173",
+    "Province": "Bangka Belitung",
+    "Provinsi": "Bangka Belitung",
+    "Nomor Telepon": "0812-7461-9599"
+  },
+  {
+    "Nama": "Damkar Lampung",
+    "Alamat": "jln. Raya Gunung Sugih - Padang Ratu, Gn. Sugih, Kec. Gn. Sugih, Kabupaten Lampung Tengah, Lampung 34161",
+    "Province": "Lampung",
+    "Provinsi": "Lampung",
+    "Nomor Telepon": "0725-25177"
+  },
+  {
+    "Nama": "Damkar DKI Jakarta",
+    "Alamat": "Jl. Kyai Haji Zainul Arifin No.71, RT.10/RW.10, Duri Pulo, Kecamatan Gambir, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10140",
+    "Province": "DKI Jakarta",
+    "Provinsi": "DKI Jakarta",
+    "Nomor Telepon": "021-6344579"
+  },
+  {
+    "Nama": "Damkar Banten",
+    "Alamat": "Komplek, Jl. Villa Melati Mas Raya Jl. Raya Serpong No.3, Pd. Jagung, Kec. Serpong Utara, Kota Tangerang Selatan, Banten 15323",
+    "Province": "Banten",
+    "Provinsi": "Banten",
+    "Nomor Telepon": "0811-900074"
+  },
+  {
+    "Nama": "Damkar Jawa Barat",
+    "Alamat": "Jl. Sukabumi No.17, Kacapiring, Kec. Batununggal, Kota Bandung, Jawa Barat 40271",
+    "Province": "West Java",
+    "Provinsi": "Jawa Barat",
+    "Nomor Telepon": "022-7207113"
+  },
+  {
+    "Nama": "Damkar Jawa Tengah",
+    "Alamat": "Jl. Madukoro Raya No.6, Krobokan, Kec. Semarang Bar., Kota Semarang, Jawa Tengah 50144",
+    "Province": "Central Java",
+    "Provinsi": "Jawa Tengah",
+    "Nomor Telepon": "024-7605871"
+  },
+  {
+    "Nama": "Damkar DIY",
+    "Alamat": "Jl. Ipda Tut Harsono, Muja Muju, Kec. Umbulharjo, Kota Yogyakarta, Daerah Istimewa Yogyakarta 55165",
+    "Province": "Special Region of Yogyakarta",
+    "Provinsi": "Daerah Istimewa Yogyakarta",
+    "Nomor Telepon": "0274-587101"
+  },
+  {
+    "Nama": "Damkar Jawa Timur",
+    "Alamat": "Jl. Pasar Turi No.21, Bubutan, Kec. Bubutan, Kota SBY, Jawa Timur 60174",
+    "Province": "East Java",
+    "Provinsi": "Jawa Timur",
+    "Nomor Telepon": "031-3533843"
+  },
+  {
+    "Nama": "Damkar Bali",
+    "Alamat": "Jl. Imam Bonjol No.184, Pemecutan Klod, Kec. Denpasar Bar., Kota Denpasar, Bali 80113",
+    "Province": "Bali",
+    "Provinsi": "Bali",
+    "Nomor Telepon": "0361-489295"
+  },
+  {
+    "Nama": "Damkar NTB",
+    "Alamat": "Jl. Bung Hatta, Cilinaya, Kec. Cakranegara, Kota Mataram, Nusa Tenggara Barat 83127",
+    "Province": "West Nusa Tenggara",
+    "Provinsi": "Nusa Tenggara Barat",
+    "Nomor Telepon": "0370-645872"
+  },
+  {
+    "Nama": "Damkar NTT",
+    "Alamat": "Jl. Mandiri II No.10, Pasir Panjang, Kec. Kota Lama, Kota Kupang, Nusa Tenggara Timur",
+    "Province": "East Nusa Tenggara",
+    "Provinsi": "Nusa Tenggara Timur",
+    "Nomor Telepon": "0380-821"
+  },
+  {
+    "Nama": "Damkar Kalimantan Barat",
+    "Alamat": "Jl. Trans Kalimantan No.89, Sul Ambawang Kuala, Kec. Sungai Ambawang, Kabupaten Kubu Raya, Kalimantan Barat 78242",
+    "Province": "West Kalimantan",
+    "Provinsi": "Kalimantan Barat",
+    "Nomor Telepon": "0813-45282341"
+  },
+  {
+    "Nama": "Damkar Kalimantan Tengah",
+    "Alamat": "Jl. P.DIPONEGORO No. 44 Palangka Raya",
+    "Province": "Central Kalimantan",
+    "Provinsi": "Kalimantan Tengah",
+    "Nomor Telepon": "0536-3231006"
+  },
+  {
+    "Nama": "Damkar Kalimantan Selatan",
+    "Alamat": "Sungai Jingah, Banjarmasin",
+    "Province": "South Kalimantan",
+    "Provinsi": "Kalimantan Selatan",
+    "Nomor Telepon": "0897-1446-599"
+  },
+  {
+    "Nama": "Damkar Kalimantan Timur",
+    "Alamat": "Jl. Ruhui Rahayu I No. Sepinggan, Baru, Balikpapan, Kalimantan Timur 76115",
+    "Province": "East Kalimantan",
+    "Provinsi": "Kalimantan Timur",
+    "Nomor Telepon": "0542- 878207"
+  },
+  {
+    "Nama": "Damkar Kalimantan Utara",
+    "Alamat": "Kp. Empat, Tarakan Tim., Kota Tarakan, Kalimantan Utara",
+    "Province": "North Kalimantan",
+    "Provinsi": "Kalimantan Utara",
+    "Nomor Telepon": "0551-32683"
+  },
+  {
+    "Nama": "Damkar Sulawesi Utara",
+    "Alamat": "Jl Balai Kota Nomor 1",
+    "Province": "North Sulawesi",
+    "Provinsi": "Sulawesi Utara",
+    "Nomor Telepon": "081-52527041"
+  },
+  {
+    "Nama": "Damkar Gorontalo",
+    "Alamat": "Jl. Jamaludin Malik, Limba U Dua, Kota Sel., Kota Gorontalo, Gorontalo 96138",
+    "Province": "Gorontalo",
+    "Provinsi": "Gorontalo",
+    "Nomor Telepon": "0435-822602"
+  },
+  {
+    "Nama": "Damkar Sulawesi Tengah",
+    "Alamat": "Kantor Dinas Pemadam Kebakaran dan Penyelamatan Kota Palu, Jalan Balai Kota Timur, Tanamodindi, Kota Palu",
+    "Province": "Central Sulawesi",
+    "Provinsi": "Sulawesi Tengah",
+    "Nomor Telepon": "0451-423113"
+  },
+  {
+    "Nama": "Damkar Sulawesi Barat",
+    "Alamat": "Karema, Kec. Mamuju, Kabupaten Mamuju, Sulawesi Barat 91512",
+    "Province": "West Sulawesi",
+    "Provinsi": "Sulawesi Barat",
+    "Nomor Telepon": "0426-21639"
+  },
+  {
+    "Nama": "Damkar Sulawesi Selatan",
+    "Alamat": "l. DR. Ratulangi No.11, Mamajang Dalam, Kec. Mamajang, Kota Makassar, Sulawesi Selatan 90113",
+    "Province": "South Sulawesi",
+    "Provinsi": "Sulawesi Selatan",
+    "Nomor Telepon": "0411-854444"
+  },
+  {
+    "Nama": "Damkar Sulawesi Tenggara",
+    "Alamat": "Jl. Balai Kota III, Pondambea, Kec. Kadia, Kota Kendari, Sulawesi Tenggara 93115",
+    "Province": "Southeast Sulawesi",
+    "Provinsi": "Sulawesi Tenggara",
+    "Nomor Telepon": "0401-3122113"
+  },
+  {
+    "Nama": "Damkar Maluku Utara",
+    "Alamat": "Jl.Cengkeh Afo Kelurahan Batu Anteru,Ternate,Ternate Tengah,Maluku Utara",
+    "Province": "North Maluku",
+    "Provinsi": "Maluku Utara",
+    "Nomor Telepon": "0921-3124113"
+  },
+  {
+    "Nama": "Damkar Maluku",
+    "Alamat": "Gedung B Lantai 1, Jl. Sultan Hairun No. 1, Kel Honipopu, Ambon, Kota Ambon, Maluku",
+    "Province": "Maluku",
+    "Provinsi": "Maluku",
+    "Nomor Telepon": "0911-351113"
+  },
+  {
+    "Nama": "Damkar Papua Barat",
+    "Alamat": "Jl. Kurana, Remu Utara, Kec. Sorong, Kota Sorong, Papua Bar. 98412",
+    "Province": "West Papua",
+    "Provinsi": "Papua Barat",
+    "Nomor Telepon": "0971-234567"
+  },
+  {
+    "Nama": "Damkar Papua",
+    "Alamat": "Asano, Abepura, Asano, Abepura, Kota Jayapura, Papua",
+    "Province": "Papua",
+    "Provinsi": "Papua",
+    "Nomor Telepon": "0813-4418-1501"
+  }
+]
---
+++ data/dataPolisi.js
@@ -0,0 +1,258 @@
+dataPolisi = [
+  {
+    "Nama": "Polda Bali",
+    "Alamat": "Jl. WR Supratman No 7 Denpasar",
+    "Nomor Telepon": "0361-227174",
+    "Provinsi": "Bali",
+    "Provinsi_eng" : "Bali",
+    "Website": "bali.polri.go.id"
+  },
+  {
+    "Nama": "Polda Bangka Belitung",
+    "Alamat": "Jl. Komplek Perkantoran Air Itam No 3 Pangkal Pinang",
+    "Nomor Telepon": "0717-437908",
+    "Provinsi": "Bangka-Belitung",
+    "Provinsi_eng" : "Bangka-Belitung",
+    "Website": "babel.polri.go.id"
+  },
+  {
+    "Nama": "Polda Banten",
+    "Alamat": "Jl. Syeh Nawawi Al Bantani No 76 Serang",
+    "Nomor Telepon": "0254-228454",
+    "Provinsi": "Banten",
+    "Provinsi_eng": "Banten",
+    "Website": "banten.polri.go.id"
+  },
+  {
+    "Nama": "Polda Bengkulu",
+    "Alamat": "Jl. Adam Malik Km 9 Bengkulu",
+    "Nomor Telepon": "0736-51041",
+    "Provinsi": "Bengkulu",
+    "Provinsi_eng": "Bengkulu",
+    "Website": "bengkulu.polri.go.id"
+  },
+  {
+    "Nama": "Polda Daerah Istimewa Yogyakarta",
+    "Alamat": "Jl. Lingkar Utama Condong Catur Yogyakarta",
+    "Nomor Telepon": "0274-885009/884444",
+    "Provinsi": "DIY Yogyakarta",
+    "Provinsi_eng": "Special Region of Yogyakarta",
+    "Website": "diy.polri.go.id"
+  },
+  {
+    "Nama": "Polda Gorontalo",
+    "Alamat": "Jl. Limboto Raya No 17 Gorontalo",
+    "Nomor Telepon": "0435-838536/838456",
+    "Provinsi": "Gorontalo",
+    "Provinsi_eng": "Gorontalo",
+    "Website": "gorontalo.polri.go.id"
+  },
+  {
+    "Nama": "Polda Jambi",
+    "Alamat": "Jl. Jend Sudirman No 45 Jambi",
+    "Nomor Telepon": "0741-22025",
+    "Provinsi": "Jambi",
+    "Provinsi_eng": "Gorontalo",
+    "Website": "jambi.polri.go.id"
+  },
+  {
+    "Nama": "Polda Jawa Barat",
+    "Alamat": "Jl. Soekarno Hatta No 748 Bandung",
+    "Nomor Telepon": "022-7800005/7800201",
+    "Provinsi": "Jawa Barat",
+    "Provinsi_eng": "West Java",
+    "Website": "jabar.polri.go.id"
+  },
+  {
+    "Nama": "Polda Jawa Tengah",
+    "Alamat": "Jl. Pahlawan No 1 Semarang",
+    "Nomor Telepon": "024-8413044",
+    "Provinsi": "Jawa Tengah",
+    "Provinsi_eng": "Central Java",
+    "Website": "jateng.polri.go.id"
+  },
+  {
+    "Nama": "Polda Jawa Timur",
+    "Alamat": "Jl. A. Yani No 116 Surabaya",
+    "Nomor Telepon": "031-8280333",
+    "Provinsi": "Jawa Timur",
+    "Provinsi_eng": "East Java",
+    "Website": "jatim.polri.go.id"
+  },
+  {
+    "Nama": "Polda Kalimantan Barat",
+    "Alamat": "Jl. A. Yani No 1 Pontianak",
+    "Nomor Telepon": "0561-734004",
+    "Provinsi": "Kalimantan Barat",
+    "Provinsi_eng": "West Kalimantan",
+    "Website": "kalbar.polri.go.id"
+  },
+  {
+    "Nama": "Polda Kalimantan Selatan",
+    "Alamat": "Jl. S. Parman No 16 Banjarmasin",
+    "Nomor Telepon": "0511-3354876",
+    "Provinsi": "Kalimantan Selatan",
+    "Provinsi_eng": "South Kalimantan",
+    "Website": "kalsel.polri.go.id"
+  },
+  {
+    "Nama": "Polda Kalimantan Tengah",
+    "Alamat": "Jl. Tjilik Riwut Km 1 Palangkaraya",
+    "Nomor Telepon": "0536-3221720",
+    "Provinsi": "Kalimantan Tengah",
+    "Provinsi_eng": "West Kalimantan",
+    "Website": "kalteng.polri.go.id"
+  },
+  {
+    "Nama": "Polda Kalimantan Timur",
+    "Alamat": "Jl. Syarifuddin Yoes No 99 Balikpapan",
+    "Nomor Telepon": "0542-421220",
+    "Provinsi": "Kalimantan Timur",
+    "Provinsi_eng": "East Kalimantan",
+    "Website": "kaltim.polri.go.id"
+  },
+  {
+    "Nama": "Polda Kepulauan Riau",
+    "Alamat": "Jl. Hang Jebat Batu Besar, Nongsa, Batam",
+    "Nomor Telepon": "0778-7763541",
+    "Provinsi": "Kepulauan Riau",
+    "Provinsi_eng": "Kepulauan Riau",
+    "Website": "kepri.polri.go.id"
+  },
+  {
+    "Nama": "Polda Lampung",
+    "Alamat": "Jl. WR Supratman No 1 Bandar Lampung",
+    "Nomor Telepon": "0721-486832",
+    "Provinsi": "Lampung",
+    "Provinsi_eng": "Lampung",
+    "Website": "lampung.polri.go.id"
+  },
+  {
+    "Nama": "Polda Maluku",
+    "Alamat": "Jl. Rijali No 1 Ambon",
+    "Nomor Telepon": "0911-352912",
+    "Provinsi": "Maluku",
+    "Provinsi_eng": "Maluku",
+    "Website": "maluku.polri.go.id"
+  },
+  {
+    "Nama": "Polda Maluku Utara",
+    "Alamat": "Jl. Kapitan Pattimura, Kalumpang, Ternate",
+    "Nomor Telepon": "0921-327045",
+    "Provinsi": "Maluku Utara",
+    "Provinsi_eng": "North Maluku",
+    "Website": "malut.polri.go.id"
+  },
+  {
+    "Nama": "Polda Metro Jakarta Raya",
+    "Alamat": "Jl. Jend Sudirman No 55 Jakarta Selatan",
+    "Nomor Telepon": "021-5234001",
+    "Provinsi": "DKI Jakarta",
+    "Provinsi_eng": "DKI Jakarta",
+    "Website": "pmj.polri.go.id"
+  },
+  {
+    "Nama": "Polda Nanggroe Aceh Darussalam",
+    "Alamat": "Jl. Cut Meutia No 25 Banda Aceh",
+    "Nomor Telepon": "0651-29556",
+    "Provinsi": "Nanggroe Aceh Darussalam",
+    "Provinsi_eng": "Aceh",
+    "Website": "nad.polri.go.id"
+  },
+  {
+    "Nama": "Polda Nusa Tenggara Barat",
+    "Alamat": "Jl. Langko No 77 Mataram",
+    "Nomor Telepon": "0370-633152",
+    "Provinsi": "Nusa Tenggara Barat",
+    "Provinsi_eng": "West Nusa Tenggara",
+    "Website": "ntb.polri.go.id"
+  },
+  {
+    "Nama": "Polda Nusa Tenggara Timur",
+    "Alamat": "Jl. Soeharto No 3 Kupang",
+    "Nomor Telepon": "0380-833132",
+    "Provinsi": "Nusa Tenggara Timur",
+    "Provinsi_eng": "East Nusa Tenggara",
+    "Website": "ntt.polri.go.id"
+  },
+  {
+    "Nama": "Polda Papua",
+    "Alamat": "Jl. Dr Samratulangi No 8 Jayapura",
+    "Nomor Telepon": "0967?-531014/533396",
+    "Provinsi": "Papua",
+    "Provinsi_eng": "Papua",
+    "Website": "papua.polri.go.id"
+  },
+  {
+    "Nama": "Polda Papua Barat",
+    "Alamat": "JL. Pahlawan Sanggeng",
+    "Nomor Telepon": "0986-211253",
+    "Provinsi": "Papua Barat",
+    "Provinsi_eng": "West Papua",
+    "Website": "papuabarat.polri.go.id"
+  },
+  {
+    "Nama": "Polda Riau",
+    "Alamat": "Jl. Jend Sudirman No 235 Pekanbaru",
+    "Nomor Telepon": "0761-31307",
+    "Provinsi": "Riau",
+    "Provinsi_eng": "Riau",
+    "Website": "riau.polri.go.id"
+  },
+  {
+    "Nama": "Polda Sulawesi Selatan dan Barat",
+    "Alamat": "Jl. Perintis Kemerdekaan Km 16 Makasar",
+    "Nomor Telepon": "0411-515101",
+    "Provinsi": "Sulawesi Selatan",
+    "Provinsi_eng": "South Sulawesi",
+    "Website": "sulselbar.polri.go.id"
+  },
+  {
+    "Nama": "Polda Sulawesi Tengah",
+    "Alamat": "Jl. Dr Samratulangi No 78 Palu",
+    "Nomor Telepon": "0451-421555",
+    "Provinsi": "Sulawesi Tengah",
+    "Provinsi_eng": "Central Sulawesi",
+    "Website": "sulteng.polri.go.id"
+  },
+  {
+    "Nama": "Polda Sulawesi Tenggara",
+    "Alamat": "Jl. Haluleo No 1 Kendari",
+    "Nomor Telepon": "0401-391555",
+    "Provinsi": "Sulawesi Tenggara",
+    "Provinsi_eng": "Southeast Sulawesi",
+    "Website": "sultra.polri.go.id"
+  },
+  {
+    "Nama": "Polda Sulawesi Utara",
+    "Alamat": "Jl. Bethesda No 62 Manado",
+    "Nomor Telepon": "0431-862019",
+    "Provinsi": "Sulawesi Utara",
+    "Provinsi_eng": "North Sulawesi",
+    "Website": "sulut.polri.go.id"
+  },
+  {
+    "Nama": "Polda Sumatera Barat",
+    "Alamat": "Jl. Sudirman No 55 Padang",
+    "Nomor Telepon": "0751-33416/811234",
+    "Provinsi": "Sumatera Barat",
+    "Provinsi_eng": "West Sumatra",
+    "Website": "sumbar.polri.go.id"
+  },
+  {
+    "Nama": "Polda Sumatera Selatan",
+    "Alamat": "Jl. Jend Sudirman Km 4,5 Palembang",
+    "Nomor Telepon": "0711-320550",
+    "Provinsi": "Sumatera Selatan",
+    "Provinsi_eng": "South Sumatra",
+    "Website": "sumsel.polri.go.id"
+  },
+  {
+    "Nama": "Polda Sumatera Utara",
+    "Alamat": "Jl. SM Raja XII Km 10,5 No 60 Medan",
+    "Nomor Telepon": "061-7879363",
+    "Provinsi": "Sumatera Utara",
+    "Provinsi_eng": "North Sumatra",
+    "Website": "sumut.polri.go.id"
+  }
+]
