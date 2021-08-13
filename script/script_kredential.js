f6ebf16e6a8163f36be7b5bdb9dc3b5d13e8a2ad
clumsyyyy <owencwijaya@gmail.com>
2021-08-13 11:00:55 +0700

    perbaikan menu, footer, vp

--- script/script_kredential.js
+++ script/script_kredential.js
@@ -8,10 +8,10 @@ function checkForStorage(){
 function putUserList(data){
     if (checkForStorage()){
         let userData = [];
-        if (localStorage.getItem(storageKey) === null){
-            userData = [];
-        } else {
+        if (localStorage.getItem(storageKey) !== ""){
             userData = JSON.parse(localStorage.getItem(storageKey));
+        } else {
+            userData = []
         }
 
         userData.unshift(data);
@@ -20,9 +20,26 @@ function putUserList(data){
         }
 
         localStorage.setItem(storageKey, JSON.stringify(userData));
+        alert("Data berhasil diperbarui!")
     }
 }
 
+function renderCurrentData(){
+    var data = []
+    if (localStorage.getItem("EMERGENSHIFT") !== ""){
+        data = JSON.parse(localStorage.getItem("EMERGENSHIFT"))[0]
+    }
+    console.log(data)
+
+    const databox = document.getElementById("databox")
+    databox.innerHTML += "<p>Data yang tersimpan: </p>"
+    databox.innerHTML += "<p>Nama: " + data.nama + "</p>"
+    databox.innerHTML += "<p>Nomor telpon: " + data.telp + "</p>"
+    databox.innerHTML += "<p>Nomor KTP: " + data.ktp + "</p>"
+    databox.innerHTML += "<p>Alamat: " + data.almt + "</p>"
+    databox.innerHTML += "<p>Provinsi: " + data.prov + "</p>"
+    databox.innerHTML += "<p>Email: " + data.email + "</p>"
+}
 submitAction.addEventListener("submit", function(event){
     const inputNama = document.getElementById("nama").value;
     const inputTelp = document.getElementById("telp").value;
@@ -41,4 +58,33 @@ submitAction.addEventListener("submit", function(event){
     }
 
     putUserList(newUserData);
+    const databox = document.getElementById("databox")
+    databox.innerHTML = ""
+    renderCurrentData(newUserData);
 })
+
+window.addEventListener("load", renderCurrentData)
+
+
+var accordion = document.getElementsByClassName("accordion");;
+    var em;
+
+    for (em = 0; em < accordion.length; em++) {
+        accordion[em].addEventListener("click", function () {
+            this.classList.toggle("active");
+
+            var panel = this.nextElementSibling;
+
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
